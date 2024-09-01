683ce7df7fa283eda22d0c139a02edfa064c749d
Jonatas Gomes Alves <jonatasgomes@gmail.com>
2024-09-01 17:15:59 -0400

    home

--- Fase 1/Atividade 1 - Startup/app.py
+++ Fase 1/Atividade 1 - Startup/app.py
@@ -1,15 +1,12 @@
 import streamlit as st
 
-if "logged_in" not in st.session_state:
-    st.session_state.logged_in = False
-
 def login():
     with st.form("login_form"):
         cliente = st.selectbox("Selecione um Cliente", ["Fazenda Boa Colheita"])
         username = st.text_input("Usuário", value="demo")
         password = st.text_input("Senha", type="password", value="demo")
 
-        if st.form_submit_button("Entrar"):
+        if st.form_submit_button("Entrar", type="primary"):
             if username == "demo" and password == "demo":
                 st.session_state.logged_in = True
                 st.rerun()
@@ -17,39 +14,45 @@ def login():
                 st.error("Invalid username or password")
 
 def logout():
-    if st.button("Sair"):
+    if st.button("Sair", type="primary"):
         st.session_state.logged_in = False
         st.rerun()
 
+# criar as páginas
 login_page = st.Page(login, title="Entrar", icon=":material/login:")
 logout_page = st.Page(logout, title="Sair", icon=":material/logout:")
-
 inicio = st.Page(
-    "page_inicio.py", title="Início", icon=":material/dashboard:", default=True
+    "page_inicio.py", title="Início", icon="🏡", default=True
 )
-culturas = st.Page("page_culturas.py", title="Culturas", icon=":material/bug_report:")
+culturas = st.Page("page_culturas.py", title="Culturas", icon="🌱")
 areas = st.Page(
-    "page_areas.py", title="Áreas de Plantio", icon=":material/notification_important:"
+    "page_areas.py", title="Áreas de Plantio", icon="📐"
 )
 insumos = st.Page(
-    "page_insumos.py", title="Manejo de Insumos", icon=":material/notification_important:"
+    "page_insumos.py", title="Manejo de Insumos", icon="⚖️"
 )
+estatisticas = st.Page("page_estatisticas.py", title="Estatísticas", icon="📈")
+meteorologia = st.Page("page_meteorologia.py", title="Meteorologia", icon="🌦️")
 
-estatisticas = st.Page("page_estatisticas.py", title="Estatísticas", icon=":material/search:")
-meteorologia = st.Page("page_meteorologia.py", title="Meteorologia", icon=":material/history:")
-
+# script para adicionar fontawesome
 st.markdown(
     '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">',
     unsafe_allow_html=True
 )
-header_container = st.container()
-with header_container:
+
+# adicionar header na aplicação
+with st.container():
     leaf = "<i class='fas fa-leaf' style='color: green;'>"
     st.markdown("<h1 style='text-align: center; color: darkgreen;'>FarmTech Solutions</h1>"
                 f"<h6 style='text-align: center; color: darkgreen; margin-top: -20px;'>{leaf} Agricultura digital => inovação + tecnologia = produtividade + sustentabilidade {leaf}</h6>",
                 unsafe_allow_html=True
     )
 
+# checar se o usuário está logado
+if "logged_in" not in st.session_state:
+    st.session_state.logged_in = False
+
+# mostrar menu lateral esquerdo
 if st.session_state.logged_in:
     pg = st.navigation(
         {
--- Fase 1/Atividade 1 - Startup/page_inicio.py
+++ Fase 1/Atividade 1 - Startup/page_inicio.py
@@ -1,8 +1,11 @@
-from streamlit_card import card
+import streamlit as st
 
-hasClicked = card(
-  title="Hello World!",
-  text="Some description",
-  image="http://placekitten.com/200/300",
-  url="https://github.com/gamcoh/st-card"
-)
+for i in range(2):
+    st.write("")
+
+with st.container(border=True):
+    st.page_link("page_culturas.py", label=":green[**$$\\underline{Culturas}$$**] - Com quais culturas você deseja trabalhar?", icon="🌱")
+    st.page_link("page_areas.py", label=":green[**$$\\underline{Áreas\\,de\\,Plantio}$$**] - Calcule as áreas de plantio para cada cultura", icon="📐")
+    st.page_link("page_insumos.py", label=":green[**$$\\underline{Manejo\\,de\\,Insumos}$$**] - Calcule as quantidades necessárias", icon="⚖️")
+    st.page_link("page_estatisticas.py", label=":green[**$$\\underline{Estatísticas}$$**] - Descubra os números de sua fazenda", icon="📈")
+    st.page_link("page_meteorologia.py", label=":green[**$$\\underline{Meteorologia}$$**] - Vai chover?", icon="🌦️")
