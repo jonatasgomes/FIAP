dc10c816c0b0d981e740b7cddf03bd6bfd4d42f8
Jonatas Gomes Alves <jonatasgomes@gmail.com>
2024-09-02 21:47:41 -0400

    home

---
+++ Fase 1/Atividade 1 - Startup/page_insumos.py
@@ -0,0 +1,28 @@
+
+
+
+# Passo a Passo para o Manejo:
+#
+# Selecionar a Cultura:
+#
+# No sistema, o usuário seleciona "Café" como a cultura para o manejo.
+# Escolher o Produto:
+#
+# O usuário escolhe "Fosfato" na lista de fertilizantes recomendados para o café.
+# Inserir a Dosagem:
+#
+# O usuário insere a dosagem recomendada de 500 mL por metro de rua.
+# Informar a Configuração da Lavoura:
+#
+# O usuário insere o número de ruas: 10 ruas.
+# O usuário insere o comprimento de cada rua: 100 metros.
+# Cálculo do Total de Insumos Necessários:
+#
+# Cálculo da Quantidade de Solução por Rua:
+# 500 mL por metro x 100 metros = 50.000 mL (ou 50 litros) de solução de fosfato por rua.
+# Cálculo do Total para Todas as Ruas:
+# 50 litros por rua x 10 ruas = 500 litros de solução de fosfato necessários para toda a área de cultivo.
+# Resultado e Ação:
+#
+# O sistema mostra que são necessários 500 litros de solução de fosfato para aplicar em todas as ruas da lavoura de café.
+# O usuário pode então planejar a compra e a aplicação dos insumos, programando a operação no campo.
